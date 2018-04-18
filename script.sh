3e021334e99eda6b39aa5dfbc898ac68c3b2e9ea
thibaut603 <thibaut.dewel@gmail.com>
2018-04-18 11:57:48 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,51 +1,76 @@
 #!/bin/bash
 
+apt-get install gcc -y
+apt-get install gcc++ -y
+apt-get install make -y
+
+apt-get install bison -y
+
+apt-get install openssl -y 
+
+apt-get install libssl -y
+
+apt-get install libssl-dev -y
+
+apt-get install libeditline0 -y
+
+apt-get install libeditline-dev -y
+
+apt-get install libedit-dev -y
+
+apt-get install libedit2 -y
+
+apt-get install libncurses5 -y
+
+apt-get install libncurses5-dev -y
+
+apt-get install zlib1g-dev -y
+
+
 #Installer build-essential
 apt-get install build-essential -y
 
 #Installer les kernel header
 apt-get install linux-headers-$(uname -r) -y
 
-#libxml2
-apt-get install libxml2 -y
-
-#libxml2-dev
-apt-get install libxml2-dev -y
-
-#libncurses5
-apt-get install libncurses5 -y
-
-apt-get install libncurses5-dev -y
-apt-get install libnewt0.52 -y
 apt-get install libnewt-dev -y
-apt-get install vim-nox -y
+
 apt-get install libsqlite3-dev -y
 
+apt-get install libsqlite3-0 -y
+
+apt-get install uuid-dev -y
+
+apt-get install uuid -y
+
+#wget asterisk
 cd /usr/local/src
+wget https://downloads.asterisk.org/pub/telephony/asterisk/asterisk-15-current.tar.gz
+tar -zxf asterisk-15-current.tar.gz
 
-wget http://downloads.asterisk.org/pub/telephony/certified-asterisk/certified-asterisk-13.1-current.tar.gz
-
-apt-get install libpri1.4 -y
-
-wget http://downloads.asterisk.org/pub/telephony/dahdi-linux-complete/dahdi-linux-complete-2.10.2-rc1+2.10.2-rc1.tar.gz
-
-tar -zxf dahdi-linux-complete-2.10.2-rc1+2.10.2-rc1.tar.gz
-tar -zxf certified-asterisk-13.1-current.tar.gz
-
-cd /usr/local/src/certified-asterisk-13.1-cert2/contrib/scripts
+#PJSIP-pjproject
+cd /usr/local/src/asterisk-15.3.0/contrib/scripts/
 ./install_prereq install
 
-cd /usr/local/src/dahdi-linux-complete-2.10.2-rc1+2.10.2-rc1
+cd /usr/local/src/asterisk-15.3.0
+./configure --with-pjproject-bundled
+
+cd /usr/local/src/
 make
 make install
 sleep 5
 make config
 
-cd /usr/local/src/certified-asterisk-13.1-cert2
+cd /usr/local/src/asterisk-15.3.0
 ./configure
 make menuselect.makeopts
-menuselect/menuselect --enable CORE-SOUNDS-FR-GSM --enable CORE-SOUNDS-FR-ULAW --enable EXTRA-SOUNDS-FR-GSM --enable EXTRA-SOUNDS-FR-ULA --enable pbx_ael --enable aelparse --enable app_meetme --enable MOH-OPSOUND-ULAW --enable chan_sip menuselect.makeopts
+menuselect/menuselect --enable CORE-SOUNDS-FR-GSM --enable CORE-SOUNDS-EN-GSM --enable CORE-SOUNDS-FR-ULAW --enable CORE-SOUNDS-EN-ULAW --enable EXTRA-SOUNDS-FR-GSM --enable EXTRA-SOUNDS-EN-GSM --enable EXTRA-SOUNDS-FR-ULAW --enable EXTRA-SOUNDS-EN-ULAW  --enable EXTRA-SOUNDS-FR-ULAW --enable EXTRA-SOUNDS-FR-ULAW   --enable aelparse --enable app_meetme --enable MOH-OPSOUND-ULAW --enable MOH-OPSOUND-GSM  --enable chan_sip menuselect.makeopts
+make
 make install
 sleep 5
 make samples
 make config
+
+sleep5
+
+/etc/init.d/asterisk status
